fcda721aa74bcab288e93b69a7524ad256bad07e
Your Name <you@example.com>
2021-06-11 05:09:18 +0300

    Veterinary Clinic V1.0

---
+++ DataAccess/Concrete/EntityFramework/VeterinaryContext.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class VeterinaryContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
+        }
+
+        //Hayvanlar Yazımı veri tabanındaki isimle aynı olmalı
+        public DbSet<Hayvan> Hayvanlar { get; set; }
+    }
+}
---
+++ Entities/Concrete/Hayvan.cs
@@ -0,0 +1,35 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Hayvan : IEntity
+    {
+
+        public int Id { get; set; }
+
+        public int MusteriId { get; set; }
+
+        public int TurId { get; set; }
+
+        public int IrkId { get; set; }
+
+        public string Adi { get; set; }
+
+        public string Kurk { get; set; }
+
+        public string OnemliHastaligi { get; set; }
+
+        public string Aciklama { get; set; }
+
+        public DateTime DogumTarihi { get; set; }
+
+        public DateTime KayitTarihi { get; set; }
+
+        public bool AktiflikDurumu { get; set; }
+
+
+    }
+}
